6ddaa466fe5fcb3713e4bfe2961f4de117c14c46
trickyhunter71 <dtb6@stmarys-ca.edu>
2026-05-12 09:44:09 -0700

    Update Hirschberg.py

--- alignment/Hirschberg.py
+++ alignment/Hirschberg.py
@@ -9,16 +9,13 @@ def read_fasta(filename):
     return sequence
 
 def nw_score(seq1, seq2):
-    """
-    Computes only the last row of the Needleman-Wunsch matrix.
-    Space complexity: O(len(seq2))
-    """
+
     match_score = 2
     mismatch_penalty = -1
     gap_penalty = -2
 
     m = len(seq2)
-    # prev_row stores the scores for the previous character in seq1
+    
     prev_row = [j * gap_penalty for j in range(m + 1)]
     curr_row = [0] * (m + 1)
 
@@ -35,9 +32,7 @@ def nw_score(seq1, seq2):
     return prev_row
 
 def hirschberg(seq1, seq2):
-    """
-    Recursive Divide and Conquer alignment.
-    """
+
     aligned1 = ""
     aligned2 = ""
 
@@ -51,20 +46,20 @@ def hirschberg(seq1, seq2):
             aligned1 += char
             aligned2 += '-'
     elif len(seq1) == 1 or len(seq2) == 1:
-        # For very small strings, use the standard NW to finish it off
+        # For very small strings, use the standard NW 
         aligned1, aligned2, _ = needleman_wunsch_basic(seq1, seq2)
     else:
-        # 1. Divide seq1 in half
+        # Divide seq1 in half
         mid1 = len(seq1) // 2
 
-        # 2. Find the score of the first half (Forward)
+        # Find  score of the first half (Forward)
         score_left = nw_score(seq1[:mid1], seq2)
         
-        # 3. Find the score of the second half (Backward)
-        # We reverse both strings to calculate scores from the end to the middle
+        # Find the score of the second half (Backward)
+        # Reverse both strings to calculate scores from the end to the middle
         score_right = nw_score(seq1[mid1:][::-1], seq2[::-1])
         
-        # 4. Find the split point in seq2 (mid2) that maximizes the total score
+        # Find the split point in seq2 (mid2) that max the total score
         # Total score at index j = score_left[j] + score_right[len(seq2) - j]
         max_v = -float('inf')
         mid2 = 0
@@ -73,7 +68,7 @@ def hirschberg(seq1, seq2):
                 max_v = score_left[j] + score_right[len(seq2) - j]
                 mid2 = j
 
-        # 5. Conquer: Recursively solve the two sub-problems
+        # Recursively solve the two sub-problems
         top_left_1, top_left_2 = hirschberg(seq1[:mid1], seq2[:mid2])
         bottom_right_1, bottom_right_2 = hirschberg(seq1[mid1:], seq2[mid2:])
         
@@ -83,9 +78,7 @@ def hirschberg(seq1, seq2):
     return aligned1, aligned2
 
 def needleman_wunsch_basic(seq1, seq2):
-    """
-    Small-scale NW used as a base case for the recursion.
-    """
+
     match_score, mismatch_penalty, gap_penalty = 2, -1, -2
     rows, cols = len(seq1) + 1, len(seq2) + 1
     score = [[0]*cols for _ in range(rows)]
@@ -96,7 +89,7 @@ def needleman_wunsch_basic(seq1, seq2):
             diag = score[i-1][j-1] + (match_score if seq1[i-1] == seq2[j-1] else mismatch_penalty)
             score[i][j] = max(diag, score[i-1][j] + gap_penalty, score[i][j-1] + gap_penalty)
     
-    # Simple traceback for small sequences
+    
     res1, res2 = "", ""
     i, j = len(seq1), len(seq2)
     final_s = score[i][j]
@@ -112,7 +105,7 @@ def needleman_wunsch_basic(seq1, seq2):
             j -= 1
     return res1, res2, final_s
 
-# --- Execution ---
+
 human_sequence = read_fasta("DNA Sequences/human_hba.fasta")
 chimp_sequence = read_fasta("DNA Sequences/chimp_hba.fasta")
 
